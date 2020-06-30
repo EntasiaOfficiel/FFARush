114ee432722be6e299e92e968d27eca857345fee
iTrooz_ <team.mortelgames@gmail.com>
2020-06-30 17:33:58 +0200

    wrong spigot version

--- src/fr/entasia/ffarush/listeners/FightListeners.java
+++ src/fr/entasia/ffarush/listeners/FightListeners.java
@@ -1,9 +1,11 @@
 package fr.entasia.ffarush.listeners;
 
+import fr.entasia.apis.other.ChatComponent;
 import fr.entasia.apis.regionManager.api.RegionManager;
 import fr.entasia.egtools.utils.MoneyUtils;
 import fr.entasia.ffarush.FFAUtils;
 import fr.entasia.ffarush.utils.FFAPlayer;
+import net.md_5.bungee.api.ChatMessageType;
 import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.entity.Player;
@@ -65,7 +67,7 @@ public class FightListeners implements Listener {
 			}
 			if(e.getFinalDamage()<p.getHealth()){
 				if(dam!=null){
-					dam.sendActionBar("§4"+(int)(p.getHealth()-e.getFinalDamage())+"§6/§420 §c❤");
+					dam.sendMessage(ChatMessageType.ACTION_BAR, ChatComponent.create("§4"+(int)(p.getHealth()-e.getFinalDamage())+"§6/§420 §c❤"));
 				}
 			}else{
 				e.setCancelled(true);
@@ -107,9 +109,9 @@ public class FightListeners implements Listener {
 			damager.sb.refreshKs();
 			damager.sb.refreshMoney();
 			ffp.p.sendMessage("§7Tu as été tué par §c"+damager.p.getName()+" ! §9( §4"+(int)damager.p.getHealth()+"§6/§420 §c❤ restants §9)");
-			ffp.p.sendActionBar("§cTué par "+damager.p.getDisplayName()+" !");
+			ffp.p.sendMessage(ChatMessageType.ACTION_BAR, ChatComponent.create("§cTué par "+damager.p.getDisplayName()+" !"));
 			damager.p.sendMessage("§7Tu as tué §c"+ffp.p.getName()+" ! §9( §4"+(int)damager.p.getHealth()+"§6/§420 §c❤ restants §9)");
-			damager.p.sendActionBar("§4"+ffp.p.getDisplayName()+" §ctué !");
+			damager.p.sendMessage(ChatMessageType.ACTION_BAR, ChatComponent.create("§4"+ffp.p.getDisplayName()+" §ctué !"));
 			damager.p.setHealth(20);
 
 			ffp.lastDamage = 0;
